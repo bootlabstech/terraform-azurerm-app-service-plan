27df258475593fe69f5fe50a8d0a6c5b967b13ad
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-09-20 11:30:55 +0530

    recheck the module

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ resource "azurerm_app_service_plan" "appservice" {
   name                = var.appservice_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  kind                = "Linux"
-  reserved            = true
+  kind                = var.kind
+  reserved            = var.reserved
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = var.sku_tier
+    size = var.sku_size
   }
 }
--- variables.tf
+++ variables.tf
@@ -12,3 +12,26 @@ variable "appservice_name" {
   type        = string
   description = "name of the azurerm_app_service_plan"
 }
+
+variable "kind" {
+  type = string
+  description = "The kind of the App Service Plan to create. Possible values are Windows (also available as App), Linux, elastic (for Premium Consumption) and FunctionApp (for a Consumption Plan). Defaults to Windows"
+}
+
+variable "reserved" {
+  type = bool
+  description = "Is this App Service Plan Reserved. Defaults to false."
+  default = true
+}
+
+variable "sku_tier" {
+  type        = string
+  description = "Specifies the plan's pricing tier(Free, Shared, Basic, Standard, Premium, PremiumV2, PremiumV3, Isolated, IsolatedV2)"
+  default     = "Standard"
+}
+
+variable "sku_size" {
+  type        = string
+  description = "Specifies the plan's instance size(Small, Medium, Large).https://learn.microsoft.com/en-us/azure/app-service/overview-hosting-plans"
+}
+
