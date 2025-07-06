b8aee061384ca36e8fee70e534341d2d12f63f1b
lalithaa2407 <lalitalallu02@gmail.com>
2025-07-07 02:44:43 +0530

    updated module provider version >4.0.0

--- provider.tf
+++ provider.tf
@@ -1,14 +1,13 @@
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.22.0"
+      version = ">= 4.0.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  skip_provider_registration = true
+
 }
